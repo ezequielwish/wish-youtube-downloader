9d300901eac14aff3657f32f08dba0191e498407
ezequielwish <ezequielalmeida.wish@gmail.com>
2022-05-13 18:26:20 -0300

    Added Thumbnail and Title viewer

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from faulthandler import disable
 import PySimpleGUI as sg
 from pytube import YouTube
 from pytube import exceptions
 import moviepy.editor as mp
 import time, os
 from threading import Thread
-
+import requests
+from PIL import Image
 
 class Video:
     def __init__(self, url):
+        self.url = url
         self.yt = YouTube(url)
     
     def download(self, quality, path):
@@ -54,34 +55,75 @@ def main_window():
     '''Configure the layout and return window object'''
     # Download Buttons layout
     in_column1 = [
-        [sg.Button('High Quality', size=(17, 4), key='-HQ-', border_width=0)],
-        [sg.Button('Low Quality', size=(17, 4), key='-LQ-', border_width=0)],
-        [sg.Button('Audio MP3', size=(17, 4), key='-MP3-', border_width=0)],
-        [sg.Button('Downloading...', size=(17, 4), key='-disHQ-', border_width=0, disabled=True)],
-        [sg.Button('Downloading...', size=(17, 4), key='-disLQ-', border_width=0, disabled=True)],
-        [sg.Button('Downloading...', size=(17, 4), key='-disMP3-', border_width=0, disabled=True)],
+        [sg.Button('High Quality', size=(15, 4), key='-HQ-', border_width=0)],
+        [sg.Button('Low Quality', size=(15, 4), key='-LQ-', border_width=0)],
+        [sg.Button('Audio MP3', size=(15, 4), key='-MP3-', border_width=0)],
+        [sg.Button('Downloading...', size=(15, 4), key='-disHQ-', border_width=0, disabled=True)],
+        [sg.Button('Downloading...', size=(15, 4), key='-disLQ-', border_width=0, disabled=True)],
+        [sg.Button('Downloading...', size=(15, 4), key='-disMP3-', border_width=0, disabled=True)],
     ]
-    # Space to show the thumbnail (not yet implemented)
+    # Space to show the thumbnail
     in_column2 = [
-        [sg.Canvas(size=(230, 230), background_color='Grey')]
+        [sg.Image(source='thumb.png', key='-thumb-', size=(330, 230))]
     ]
     # Layout for the Frame (Select the destination folder)
     select_folder = [
         [
-            sg.Text(size=(40, 1), key='-folder-', border_width=0), 
+            sg.Text(size=(50, 1), key='-folder-', border_width=0), 
             sg.Button(button_text='Browse', key='-select-', border_width=0)
         ]
     ]
     # Main layout
     layout = [
-        [sg.Image('logo.png', size=(400, 100))],
-        [sg.Input(size=(49, 1), key='-url-', border_width=0), sg.Button(button_text='Paste', key='-paste-', border_width=0)],
+        [sg.Image('logo.png', size=(480, 100))],
+        [
+            sg.Input(size=(61, 1), key='-url-', border_width=0), 
+            sg.Button(button_text='Done!', key='-paste-', border_width=0, bind_return_key=True)
+        ],
         [sg.Column(layout=in_column1), sg.Column(layout=in_column2)],
+        [sg.Text(key='-title-', size=(59, 1), justification='center')],
         [sg.Frame(title='Destination folder', title_location='n', layout=select_folder)],
-        [sg.Text('Developed by Ezequiel Almeida', size=(50, 1), justification='center', text_color='grey')]
+        [sg.Text('Developed by Ezequiel Almeida', size=(60, 1), justification='center', text_color='grey')]
     ]
     # Return the window as Object
     return sg.Window(title='Wish Youtube Downloader', layout = layout, finalize=True)
+
+
+def set_thumbnail_and_title(video):
+    '''Show thumbnail image and title on the interface'''
+    video_url = video.url
+    video_title = video.yt.title
+    download_thumbnail(video_url)
+    window['-thumb-'].update(source='temp.png', size=(330, 230))
+    window['-title-'].update(video_title)
+
+
+def download_thumbnail(url):
+    '''Download and prepare the thumbnail image to show on the interface'''
+    img_data = requests.get(YouTube(url).thumbnail_url).content
+    with open('temp.png', 'wb') as handler:
+        handler.write(img_data)
+    compress_image('temp.png')
+
+
+def compress_image(path):
+    '''Compress and resize a image to appear in the interface'''
+    image = Image.open(path)
+    image.resize(size=(330, 230), reducing_gap=Image.ANTIALIAS)
+    image.save(path, optimize=True, quality=95)
+
+
+def set_video(url):
+    '''Set video throught a valid url'''
+    global window
+    try:
+        video = Video(url)
+    except exceptions.RegexMatchError: # Raise RegexMatchError if the url is a invalid youtube url so...
+        sg.popup('Invalid Url!')
+        window['-url-'].update('') # Clear the Url Field
+    else:
+        set_thumbnail_and_title(video)
+        return video
 
 
 sg.theme('Black') # Set theme
@@ -100,15 +142,14 @@ while True:
     elif event == '-select-': # If the user click on the Browse button
         path = sg.PopupGetFolder(no_window=True, message='')
     elif event == '-paste-': # If the user click on the Paste button
-        url = sg.clipboard_get() # Paste the current clipboard data on the Input
-        window['-url-'].update(url)
-        # *Thumb change here*
+        if values['-url-'] == '':
+            url = sg.clipboard_get()
+            window['-url-'].update(url) # Paste the current clipboard data on the Input
+            video = set_video(url) # 
+        else: 
+            video = set_video(values['-url-']) # Try to set the video with the current url on the -url- field
     else:
-        '''If the user click in an Download button'''
-        try:
-            video = Video(values['-url-'])
-            # Download the video in the quality of the clicked button and in the place selected
-            download_thread = Thread(target=video.download, args=(event, path)).start() 
-        except exceptions.RegexMatchError: # Raise RegexMatchError if the url is a invalid youtube url so...
-            sg.popup('Invalid URL!')
-            window['-url-'].update('') # Clear the Url Field
+        '''If the user click in a Download button'''
+        video = set_video(values['-url-'])
+        # Download the video in the quality of the clicked button and in the place selected
+        start_download = Thread(target=video.download, args=(event, path)).start()
