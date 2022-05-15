2aee7d5cdb43bf873ced4d1eabf7c31017f3d8b4
ezequielwish <ezequielalmeida.wish@gmail.com>
2022-05-15 18:32:13 -0300

    Bug fix and modernized interface

--- main.py
+++ main.py
@@ -11,44 +11,67 @@ class Video:
     def __init__(self, url):
         self.url = url
         self.yt = YouTube(url)
-    
+
+    @staticmethod
+    def format_title(title):
+        '''Turns the video title valid to Windows OS filenames'''
+        new = []
+        for char in title: # Separate only valid characters and put on the list
+            if char == '|':
+                new.append('-')
+            elif char.lower() in (r'aáàãâbcdeéèêfghiíìjklmnopqrstuvwxyz0987654321([{}])/$& ~-".,:;?'):
+                new.append(char)
+        new = ''.join(new) # Convert list on a string
+        if len(new) <= 50: # 50 charactere is a good lenght to filenames
+            return new
+        else:
+            return new[:51]+'...' # Return only the first 50 characteres to bypass Filename Lenght eror
+
     def download(self, quality, path):
         '''Download the video in High Quality, Low Quality or MP3'''
         global window
         # Change the buttons to DISABLED buttons
-        window['-HQ-'].hide_row()
-        window['-LQ-'].hide_row()
-        window['-MP3-'].hide_row()
-        window['-disHQ-'].unhide_row()
-        window['-disLQ-'].unhide_row()
-        window['-disMP3-'].unhide_row()
-        if quality == '-MP3-':
-            '''The button pressed is Audio MP3, so download as audio .mp4 and convert to .mp3'''
-            audio = self.yt.streams.get_audio_only()
-            title = str(audio.title)
-            audio.download(path, filename=f'{title}.mp4') # Download in the specified folder (path)
-            mp3 = mp.AudioFileClip(f'{path}/{title}.mp4') # Set the .mp4 audio
-            time.sleep(0.5)
-            mp3.write_audiofile(f'{path}/{title}.mp3') # Create a .mp3
-            os.remove(f'{path}/{title}.mp4') # Delete the .mp4
-        elif quality == '-HQ-':
-            '''Select the highest resolution and download them'''
-            video = self.yt.streams.get_highest_resolution()
-            title = str(video.title)
-            video.download(path, filename=f'{title} [HQ].mp4')
-        elif quality == '-LQ-':
-            '''Select the lowest resolution and download them'''
-            video = self.yt.streams.get_lowest_resolution()
-            title = str(video.title)
-            video.download(path, filename=f'{title} [LQ].mp4')
-        # Change the buttons to ENABLED buttons again
-        window['-HQ-'].unhide_row()
-        window['-LQ-'].unhide_row()
-        window['-MP3-'].unhide_row()
-        window['-disHQ-'].hide_row()
-        window['-disLQ-'].hide_row()
-        window['-disMP3-'].hide_row()
-        print(f'Download Done!')
+        try:
+            window['-HQ-'].hide_row()
+            window['-LQ-'].hide_row()
+            window['-MP3-'].hide_row()
+            window['-disHQ-'].unhide_row()
+            window['-disLQ-'].unhide_row()
+            window['-disMP3-'].unhide_row()
+            filename = self.format_title(self.yt.title[:])
+            window['-status-'].update('Downloading...')
+            if quality == '-MP3-':
+                '''The button pressed is Audio MP3, so download as audio .mp4 and convert to .mp3'''
+                video = self.yt.streams.get_audio_only()
+                video.download(path, filename=f'{filename}.mp4') # Download in the specified folder (path)
+                window['-status-'].update('Converting to mp3...')
+                mp3 = mp.AudioFileClip(f'{path}/{filename}.mp4') # Set the .mp4 audio
+                time.sleep(0.5)
+                mp3.write_audiofile(f'{path}/{filename}.mp3', verbose=False) # Create a .mp3
+                time.sleep(0.5)
+                os.remove(f'{path}/{filename}.mp4') # Delete the .mp4
+                window['-status-'].update('Download sucessful! [MP3]')
+            elif quality == '-HQ-':
+                '''Select the highest resolution and download them'''
+                video = self.yt.streams.get_highest_resolution()
+                video.download(path, filename=f'{filename} [HQ].mp4')
+                window['-status-'].update('Download sucessful! [HQ]')
+            elif quality == '-LQ-':
+                '''Select the lowest resolution and download them'''
+                video = self.yt.streams.get_lowest_resolution()
+                video.download(path, filename=f'{filename} [LQ].mp4')
+                window['-status-'].update('Download sucessful! [HQ]')
+        except PermissionError:
+            window['-status-'].update('Permission Error! You have to give admin permission to save in that folder!')
+        finally:
+            # Change the buttons to ENABLED buttons again
+            window['-disHQ-'].hide_row()
+            window['-disLQ-'].hide_row()
+            window['-disMP3-'].hide_row()
+            window['-HQ-'].unhide_row()
+            window['-LQ-'].unhide_row()
+            window['-MP3-'].unhide_row()
+        
 
 
 def main_window():
@@ -64,24 +87,25 @@ def main_window():
     ]
     # Space to show the thumbnail
     in_column2 = [
-        [sg.Image(source='thumb.png', key='-thumb-', size=(330, 230))]
+        [sg.Image(source='thumb.png', key='-thumb-', size=(350, 230), pad=0)]
     ]
     # Layout for the Frame (Select the destination folder)
     select_folder = [
         [
             sg.Text(size=(50, 1), key='-folder-', border_width=0), 
-            sg.Button(button_text='Browse', key='-select-', border_width=0)
+            sg.Button(button_text='Browse', key='-path-', border_width=0)
         ]
     ]
     # Main layout
     layout = [
-        [sg.Image('logo.png', size=(480, 100))],
+        [sg.Image('logo.png', size=(480, 100), pad=0)],
         [
             sg.Input(size=(61, 1), key='-url-', border_width=0), 
             sg.Button(button_text='Done!', key='-paste-', border_width=0, bind_return_key=True)
         ],
-        [sg.Column(layout=in_column1), sg.Column(layout=in_column2)],
         [sg.Text(key='-title-', size=(59, 1), justification='center')],
+        [sg.Column(layout=in_column1, pad=0), sg.Column(layout=in_column2, pad=0)],
+        [sg.Text(key='-status-', size=(59, 1), justification='center')],
         [sg.Frame(title='Destination folder', title_location='n', layout=select_folder)],
         [sg.Text('Developed by Ezequiel Almeida', size=(60, 1), justification='center', text_color='grey')]
     ]
@@ -92,9 +116,12 @@ def main_window():
 def set_thumbnail_and_title(video):
     '''Show thumbnail image and title on the interface'''
     video_url = video.url
-    video_title = video.yt.title
+    if len(video.yt.title) > 67:
+        video_title = video.yt.title[:66] + '...'
+    else:
+        video_title = video.yt.title
     download_thumbnail(video_url)
-    window['-thumb-'].update(source='temp.png', size=(330, 230))
+    window['-thumb-'].update(source='temp.png', size=(350, 230))
     window['-title-'].update(video_title)
 
 
@@ -139,8 +166,10 @@ while True:
     event, values = window.Read()
     if event == sg.WIN_CLOSED: # If the user closes the window
         break
-    elif event == '-select-': # If the user click on the Browse button
-        path = sg.PopupGetFolder(no_window=True, message='')
+    elif event == '-path-': # If the user click on the Browse button
+        new_path = sg.PopupGetFolder(no_window=True, message='')
+        if new_path != '':
+            path = new_path
     elif event == '-paste-': # If the user click on the Paste button
         if values['-url-'] == '':
             url = sg.clipboard_get()
